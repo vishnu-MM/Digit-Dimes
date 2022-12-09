7bce8ab531524b81b689f5037d2c4d73f1fff1dc
Vishnu.M.M <vishnumm2003@gmail.com>
2022-12-09 22:08:28 +0530

    staff,catagory & product- view,add,edit,delete& search completed

--- coding.py
+++ coding.py
@@ -1,24 +1,53 @@
 from flask import *
 from DBConnection import Db
 app=Flask(__name__)
+app.secret_key="ghhjggm"
+
+
+
 
 
 @app.route('/')
 def login():
     return render_template("login.html")
 
+
+
 @app.route('/login_post',methods=['post'])
 def login_post():
     uname = request.form['textfield']
     passwrd = request.form['textfield2']
-    return render_template("login.html")
+    db=Db()
+    qry="select * from login where username='"+uname+"' and password='"+passwrd+"'"
+    res=db.selectOne(qry)
+    print(res)
+    if res is not None:
+        session['lid']=res['lid']
+        if res['type']=='admin':
+            return redirect('/Home')
+        elif res['type']=='manufacture':
+            return redirect('/Home_Manufactors')
+        else:
+            return "ok"
+    else:
+        return "okk"
+
+
 
 
 #-----------------------------admin
 
+
+
+
+
+
 @app.route('/Home')
 def Home():
     return render_template("Admin/Home.html")
+
+
+
 
 
 @app.route('/approved_manufactures')
@@ -38,6 +67,10 @@ def approved_manufactures_post():
     return render_template("Admin/Approved Manufactures.html",data=res)
 
 
+
+
+
+
 @app.route('/Change_password')
 def Change_password():
     return render_template("Admin/ChangePassword.html")
@@ -50,6 +83,11 @@ def Change_passwor_post():
     confirm_pass = request.form['textfield3']
     return render_template("Admin/ChangePassword.html")
 
+
+
+
+
+
 @app.route('/new_complaint')
 def new_complaint():
     db = Db()
@@ -57,14 +95,22 @@ def new_complaint():
     res = db.select(qry)
     return render_template("Admin/NewComplaint.html",data=res)
 
+
 @app.route('/new_complaint_post',methods=['post'])
 def new_complaint_post():
     pro_name = request.form['textfield']
     return render_template("Admin/NewComplaint.html")
 
+
+
+
+
+
 @app.route('/replay/<id>')
 def replay(id):
     return render_template("Admin/Replay.html",rid=id)
+
+
 
 @app.route('/replay_post',methods=['post'])
 def replay_post():
@@ -77,6 +123,9 @@ def replay_post():
 
 
 
+
+
+
 @app.route('/verifing_manufactiores')
 def verifing_manufactiores():
     db = Db()
@@ -84,12 +133,15 @@ def verifing_manufactiores():
     res = db.select(qry)
     return render_template("Admin/VerifingManufactores.html",data=res)
 
+
+
 @app.route('/approve_manufactiores/<id>')
 def approve_manufactiores(id):
     qry="UPDATE `manufacturer` SET STATUS='approved' WHERE man_id='"+id+"'"
     db=Db()
     res=db.update(qry)
     return '''<script>alert('approved successfully');window.location='/verifing_manufactiores'</script>'''
+
 
 
 @app.route('/reject_manufactiores/<id>')
@@ -100,10 +152,17 @@ def reject_manufactiores(id):
     return '''<script>alert('Rejected Successfully');window.location='/verifing_manufactiores'</script>'''
 
 
+
+
 @app.route('/verifing_manufactiores_post',methods=['post'])
 def verifing_manufactiores_post():
     name = request.form['textfield']
-    return render_template("Admin/VerifingManufactores.html")
+    db=Db()
+    qry=" SELECT * FROM manufacturer  WHERE STATUS='pending' AND NAME LIKE '%"+name+"%' "
+    res=db.select(qry)
+    return render_template("Admin/VerifingManufactores.html",data=res)
+
+
 
 
 
@@ -116,6 +175,7 @@ def view_user():
     return render_template("Admin/ViewUser.html",data=res)
 
 
+
 @app.route('/view_user_post',methods=['post'])
 def view_user_post():
     name = request.form['textfield']
@@ -124,11 +184,17 @@ def view_user_post():
     res = db.select(qry)
     return render_template("Admin/ViewUser.html",data=res)
 
-#-----------------------------------------------------------------------------
+
+
+
+
+#-------------------------------------------------------------manufacters
+
+
 @app.route('/catagory_managment')
 def catagory_managment():
     db = Db()
-    qry = "SELECT * FROM category"
+    qry = "SELECT * FROM category where `man_lid`='"+str(session['lid'])+"'"
     res = db.select(qry)
     return render_template("Manufacters/CatagoryManagment.html",data=res)
 
@@ -136,7 +202,59 @@ def catagory_managment():
 @app.route('/catagory_managment_post',methods=['post'])
 def catagory_managment_post():
     name = request.form['textfield']
-    return render_template("Manufacters/CatagoryManagment.html")
+    db = Db()
+    qry = " SELECT * FROM category where `man_lid`='"+str(session['lid'])+"' and `category` LIKE '"+name+"' "
+    res = db.select(qry)
+    return render_template("Manufacters/CatagoryManagment.html",data=res)
+
+
+
+
+@app.route('/Catagory_Managment_Add')
+def Catagory_Managment_Add():
+    return render_template('Manufacters/Catagory Managment Add.html')
+
+
+@app.route('/Catagory_Managment_Add_post',methods=['post'])
+def Catagory_Managment_Add_post():
+    CataName = request.form['textfield']
+    db = Db()
+    qry = "INSERT INTO category(`category`,man_lid )VALUES('"+CataName+"','"+str(session['lid'])+"')"
+    res = db.insert(qry)
+    return redirect('/catagory_managment')
+
+
+
+
+@app.route('/catagory_managment_delete/<id>')
+def catagory_managment_add(id):
+    db = Db()
+    qry = "DELETE FROM `category` WHERE `category-id` ='"+id+"'"
+    res = db.delete(qry)
+    return "<script>alert('deleted');window.location='/catagory_managment'</script>"
+
+
+
+
+@app.route('/Catagory_Managment_edit/<id>')
+def Catagory_Managment_edit(id):
+    db = Db()
+    qry = " select * from category WHERE  `category-id`= '"+id+"'"
+    res = db.selectOne(qry)
+    return render_template('Manufacters/Catagory Managment Edit.html',data=res)
+
+
+@app.route('/Catagory_Managment_edit_post',methods=['post'])
+def Catagory_Managment_edit_post():
+    CataName = request.form['textfield']
+    Id = request.form['id']
+    db = Db()
+    qry = " UPDATE `category` SET `category`='"+CataName+"' WHERE `category-id`='"+Id+"' "
+    res = db.update(qry)
+    return '''<script>alert("updated");window.location='/catagory_managment'</script>'''
+
+
+
 
 
 
@@ -155,36 +273,100 @@ def delivary_rating():
 
 
 
-@app.route('/product_managment_add')
-def product_managment_add():
+
+@app.route('/product_managment')
+def product_managment():
     db = Db()
-    qry = " "
+    qry = "SELECT * FROM `product` JOIN `category`ON `category`.`category-id`=`product`.`categort-id` WHERE `man_id`='"+str(session['lid'])+"' "
     res = db.select(qry)
-    return render_template("Manufacters/ProductManagment-Add.html",data=res)
+    return render_template("Manufacters/ProductManagment.html",data=res)
 
 
-@app.route('/product_managment_add-_post',methods=['post'])
-def product_managment_add_post():
+
+
+@app.route('/product_managment_post',methods=['post'])
+def product_managment_post():
     pro_name = request.form['textfield']
-    return render_template("Manufacters/ProductManagment-Add.html")
+    db = Db()
+    qry = " SELECT * FROM `product`JOIN `category`ON `category`.`category-id`=`product`.`categort-id` WHERE `man_id`='"+str(session['lid'])+"' and `product_name`= '"+pro_name+"'  "
+    res = db.select(qry)
+    return render_template("Manufacters/ProductManagment.html",data = res)
 
 
 
-@app.route('/Product_Managment_edit',)
-def Product_Managment_edit():
-    return render_template("Manufacters/ProductManagment-Edit.html")
+
+@app.route('/Product_Managment_edit/<id>')
+def Product_Managment_edit(id):
+    db = Db()
+    qry="SELECT * FROM `category`"
+    res=db.select(qry)
+    qry2 = "SELECT * FROM `product`  JOIN `category`ON `category`.`category-id`=`product`.`categort-id` WHERE `pid`='"+id+"'"
+    res1=db.selectOne(qry2)
+    return render_template("Manufacters/ProductManagment-Edit.html",data=res1,data2=res)
+
 
 
 
 @app.route('/Product_Managment_edit_post',methods=['post'])
 def Product_Managment_edit_post():
+    Id = request.form['id']
     img = request.files['fileField']
     pro_name = request.form['textfield']
     catgry = request.form['textfield2']
     price = request.form['textfield3']
     qty = request.form['textfield4']
     discr = request.form['textarea']
-    return render_template("Manufacters/ProductManagment-Edit.html")
+    from _datetime import datetime
+    date = datetime.now().strftime("%d%M%y-%H%M%S")
+    img.save("C:\\Users\\vishn\\PycharmProjects\\digit dimes\\static\\product\\" + date + ".jpg")
+    path = "/static/product/" + date + ".jpg"
+    db = Db()
+    qry = " UPDATE `product` SET `photo`='"+path+"',`product_name`='"+pro_name+"',`price`='"+str(price)+"',`qty`='"+str(qty)+"',`categort-id`='"+catgry+"',`description`='"+discr+"' WHERE `pid`='"+Id+"' "
+    res = db.update(qry)
+    return redirect('/product_managment')
+
+
+
+
+
+@app.route('/product_managment_addnew')
+def product_managment_addnew():
+    db = Db()
+    qry = " select * from category WHERE man_lid='"+str(session['lid'])+"'  "
+    res = db.select(qry)
+
+    return render_template("Manufacters/Product managment-Add new.html",data = res)
+
+
+
+
+@app.route('/product_managment_addnew_post',methods=['post'])
+def product_managment_addnew_post():
+    imge = request.files['fileField']
+    Pname = request.form['textfield']
+    Ctgry = request.form['textfield2']
+    price = request.form['textfield3']
+    qty = request.form['textfield4']
+    description = request.form['textarea']
+    from _datetime import datetime
+    date=datetime.now().strftime("%d%M%y-%H%M%S")
+    imge.save("C:\\Users\\vishn\\PycharmProjects\\digit dimes\\static\\product\\"+date+".jpg")
+    path="/static/product/"+date+".jpg"
+    db = Db()
+    qry = " INSERT INTO `product` (`photo`,`product_name`,`description`,`price`,`qty`,`categort-id`,`man_id`)VALUES('"+path+"','"+Pname +"','"+description+"','"+str(price)+"','"+ str(qty)+"','"+Ctgry+"','"+str(session['lid'])+"' ) "
+    res = db.insert(qry)
+    return redirect('/product_managment')
+
+
+
+@app.route('/product_managment_delete/<id>')
+def product_managment_delete(id):
+    db = Db()
+    qry = " DELETE FROM `product` WHERE `pid`='"+id+"' "
+    res = db.delete(qry)
+    return "<script>alert('deleted');window.location='/product_managment'</script>"
+
+
 
 
 
@@ -196,9 +378,13 @@ def product_review():
 
 
 
+
+
 @app.route('/profile_managment')
 def profile_managment():
     return render_template("Manufacters/ProfileManagment.html")
+
+
 
 
 @app.route('/profile_managment_post',methods=['post'])
@@ -208,6 +394,7 @@ def profile_managment_post():
     ph = request.form['textfield3']
     ema =  request.form['textfield4']
     return render_template("Manufacters/ProfileManagment.html")
+
 
 
 
@@ -230,18 +417,39 @@ def sign_up_post():
     pin  = request.form['textfield7']
     Latitude = request.form['textfield8']
     Longitude = request.form['textfield9']
-    return render_template("Manufacters/Signup.html")
+    passwd = request.form['textfield10']
+    passwdC = request.form['textfield11']
+    db=Db()
+    if passwd==passwdC:
+        qry="INSERT INTO `login`(`username`,`password`,`type`)VALUES('"+ema+"','"+passwd+"','manufacture')"
+        res=db.insert(qry)
+        qry2="INSERT INTO `manufacturer` (`man_lid`,`name`,`propreitir_name`,`email`,`phone`,`place`,`post`,`pin`,`latitude`,`longitude`,`status`) VALUES('"+str(res)+"','"+usrname+"','"+propri_name+"','"+ph+"','"+ema+"','"+place+"','"+pst+"','"+pin+"','"+Latitude+"','"+Longitude+"','pending')"
+        res=db.insert(qry2)
+        return redirect('/')
+    else:
+        "ok"
+
+
+
 
 
 
 @app.route('/staff_managment')
 def staff_managment():
-    return render_template("Manufacters/StaffManagment-0.html")
+    db= Db()
+    qry = "SELECT * FROM `staff` WHERE `man-id`= '"+str(session['lid'])+"' "
+    res = db.select(qry)
+    return render_template("Manufacters/StaffManagment-0.html",data=res)
+
 
 @app.route('/staff_managment_post',methods=['post'])
 def staff_managment_post():
     Search = request.form['textfield']
-    return render_template("Manufacters/StaffManagment-0.html")
+    db=Db()
+    qry="SELECT * FROM `staff` WHERE `man-id`='"+str(session['lid'])+"' and sname like '"+Search+"'"
+    res=db.select(qry)
+    return render_template("Manufacters/StaffManagment-0.html",data=res)
+
 
 
 
@@ -250,11 +458,8 @@ def staff_managment_add():
     return render_template("Manufacters/StaffManagment-AddNew.html")
 
 
-
-
 @app.route('/staff_managment_add_post',methods=['post'])
 def staff_managment_add_post():
-    #syntax to get value from a check box???
     Gender= request.form['checkbox']
     Name = request.form['textfield']
     age = request.form['textfield2']
@@ -264,42 +469,55 @@ def staff_managment_add_post():
     pin = request.form['textfield6']
     Post = request.form['textfield7']
     city = request.form['textfield8']
-    return render_template("Manufacters/StaffManagment-AddNew.html")
-
-
-
-@app.route('/product_managment_addnew')
-def product_managment_addnew():
-    return render_template("Manufacters/Product managment-Add new.html")
-
-
-
-
-@app.route('/product_managment_addnew_post',methods=['post'])
-def product_managment_addnew_post():
-    imge = request.files['fileField']
-    Pname = request.form['textfield']
-    Ctgry = request.form['textfield2']
-    price = request.form['textfield3']
-    qty = request.form['textfield4']
-    description = request.form['textarea']
-    return render_template("Manufacters/Product managment-Add new.html")
-
-
-
-
-@app.route('/Catagory_Managment_Add')
-def Catagory_Managment_Add():
-    return render_template('Manufacters/Catagory Managment Add.html')
+    db=Db()
+    qry = "INSERT INTO `login`(`username`,`password`,`type`)VALUES('"+ema+"','"+phone+"','staff')"
+    res=db.insert(qry)
+    qry2 = "INSERT INTO `staff`(`staff-lid`,`man-id`,`sname`,`age`,`gender`,`place`,`post`,`pin`,`city`,`phone`,`email`) VALUES('"+str(res)+"','"+str(session['lid'])+"','"+Name+"','"+age+"','"+Gender+"','"+place+"','"+Post+"','"+pin+"','"+city+"','"+phone+"','"+ema+"')"
+    res=db.insert(qry2)
+    return redirect('/staff_managment')
 
 
 
 
 
-@app.route('/Catagory_Managment_Add_post',methods=['post'])
-def Catagory_Managment_Add_post():
-    CataName = request.form['textfield']
-    return render_template('Manufacters/Catagory Managment Add.html')
+
+@app.route('/StaffManagment_Edit/<id>')
+def StaffManagment_Edit(id):
+    db = Db()
+    qry = "Select * from staff where staff_='"+id+"'"
+    res = db.selectOne(qry)
+    return render_template("Manufacters/StaffManagment-Edit.html",data=res)
+
+
+
+@app.route('/StaffManagment_Edit_post',methods=['post'])
+def StaffManagment_Edit_post():
+    Id = request.form['ID']
+    Gender = request.form['radio']
+    Name = request.form['textfield']
+    age = request.form['textfield2']
+    phone = request.form['textfield3']
+    ema = request.form['textfield4']
+    place = request.form['textfield5']
+    pin = request.form['textfield6']
+    Post = request.form['textfield7']
+    city = request.form['textfield8']
+    db = Db()
+    qry = " UPDATE `staff` SET `sname`='"+Name+"' ,`age`='"+age+"' ,`gender`='"+Gender+"' ,`place`='"+place+"',`post`='"+Post+"',`pin`='"+pin+"',`city`='"+city+"',`phone`='"+phone+"',`email`='"+ema+"' WHERE staff_='"+Id+"'"
+    res = db.update(qry)
+    return redirect('/staff_managment')
+
+
+
+
+@app.route('/Staff_management_Delete/<id>')
+def Staff_management_Delete(id):
+    db = Db()
+    qry = "DELETE FROM `staff` WHERE `staff_`='"+id+"'"
+    res = db.delete(qry)
+    return "<script>alert('deleted');window.location='/staff_managment'</script>"
+
+
 
 
 
@@ -307,13 +525,6 @@ def Catagory_Managment_Add_post():
 @app.route('/Home_Manufactors')
 def Home_Manufactors():
     return render_template("Manufacters/Home_Manufactors.html")
-
-
-
-
-
-
-
 
 
 
